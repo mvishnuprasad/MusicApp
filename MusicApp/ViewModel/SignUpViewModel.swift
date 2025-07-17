bc87577ee3fa56050db2b7cd5f067e63f7bee86c
vishnuprasad <vpm735@gmail.com>
2025-07-17 13:49:40 +0530

    added unit test using mock url protocol

--- MusicApp/ViewModel/SignUpViewModel.swift
+++ MusicApp/ViewModel/SignUpViewModel.swift
@@ -7,7 +7,16 @@
 
 import Foundation
 class SignUpViewModel {
+    
+    private let networkService: SignUpNetworkService
+    init(networkService: SignUpNetworkService) {
+        self.networkService = networkService
+    }
     func signUp() {
+        let req = SignUpRequest(name: "", password: "", email: "")
+        networkService.signUp(with: req) { response, error in
+            
+        }
         
     }
 }
